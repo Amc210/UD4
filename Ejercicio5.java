561f08000633430fd0f0abdf9a548959952e61b0
Amc210 <annamarin513@gmail.com>
2021-07-07 14:45:09 +0200

    Ejercicio

--- Ejercicio5.java
+++ Ejercicio5.java
@@ -3,19 +3,21 @@ public class Ejercicio5 {
 
 	public static void main(String[] args) {
 		//Declaración variables i asignación del 1er valor
-		int A = 5, B = 10, C = 15, D = 20;
+		int A = 5, B = 10, C = 15, D = 20, aux= 0;
 		
 		//Asignamos el valor de una variable a la otra
+		aux = B;
 		B = C;
 		C = A;
 		A = D;
-		D = B;
+		D = aux;
 		
 		//Mostramos los nuevos valores
-		System.out.println("A = " + A);
-		System.out.println("B = " + B);
-		System.out.println("C = " + C);
+		System.out.println("B = " + B);		
+		System.out.println("C = " + C);		
+		System.out.println("A = " + A);		
 		System.out.println("D = " + D);
+		
 	}
 
 }
